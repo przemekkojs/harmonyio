184f245fca61e353cf0adaf69b0da8aba703112d
przemekkojs <przemek.kojs@gmail.com>
2024-10-15 23:05:59 +0200

    Rules added
    Added classes for rules, without implementation

---
+++ algorithm/Algorithm/Algorithm/Rules/VoiceCrossing.cs
@@ -0,0 +1,14 @@
+﻿using Algorithm.Music;
+
+namespace Algorithm.Algorithm.Rules
+{
+    public class VoiceCrossing : Rule
+    {
+        public VoiceCrossing() : base("Voice Crossing", "", 1) { }
+
+        public override bool IsSatisfied(params Stack[] stacks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
