800ccdb63cc1c50891b5c23e39524a83e25c55d4
przemekkojs <przemek.kojs@gmail.com>
2024-10-22 15:35:36 +0200

    Function creator prototype

---
+++ main/Main/Components/FunctionCreator.cs
@@ -0,0 +1,21 @@
+﻿namespace Main.Components
+{
+    public record FunctionCreator
+    {
+        public string Symbol { get; set; } = "";
+        public string Root { get; set; } = "";
+        public string Position { get; set; } = "";
+        public string Added1 { get; set; } = "";
+        public string Added2 { get; set; } = "";
+        public string Added3 { get; set; } = "";
+        public string Removed { get; set; } = "";
+        public string Alteration1 { get; set; } = "";
+        public string Alteration2 { get; set; } = "";
+        public string Alteration3 { get; set; } = "";
+        public string Alteration4 { get; set; } = "";
+        public string Suspension1 { get; set; } = "";
+        public string Suspension2 { get; set; } = "";
+        public string Suspension3 { get; set; } = "";
+        public string Suspension4 { get; set; } = "";
+    }
+}
