886fdeb3512b474a1f63243db833d2255fd2313a
przemekkojs <przemek.kojs@gmail.com>
2024-10-24 23:27:19 +0200

    Functionality update
    Adding and removing tasks is finally working

--- main/Main/wwwroot/js/creator-scripts/taskCreator.js
+++ main/Main/wwwroot/js/creator-scripts/taskCreator.js
@@ -5,52 +5,49 @@ const tasks = [];
 export function createTask(parent, id) {
     let taskHtml = `        
         <div id="box-task-${id}" class="box">
-            <form>
-                <div class="params">
-                    <label for="metre-select-task-${id}">Metrum: </label>        
-                    <select id="metre-select-task-${id}">
-                        <option value="2/4">2/4</option>
-                        <option value="3/4">3/4</option>
-                        <option value="4/4">4/4</option>
-                        <option value="3/8">3/8</option>
-                        <option value="6/8">6/8</option>
-                    </select>
+            <div class="params">
+                <label for="metre-select-task-${id}">Metrum: </label>        
+                <select id="metre-select-task-${id}">
+                    <option value="2/4">2/4</option>
+                    <option value="3/4">3/4</option>
+                    <option value="4/4">4/4</option>
+                    <option value="3/8">3/8</option>
+                    <option value="6/8">6/8</option>
+                </select>
                         
-                    <label for="tonation-name-select-task-${id}">Tonacja: </label>
-                    <select id="tonation-name-select-task-${id}" class="smaller-select">
-                        <option value="C">C</option>
-                        <option value="D">D</option>
-                        <option value="E">E</option>
-                        <option value="F">F</option>
-                        <option value="G">G</option>
-                        <option value="A">A</option>
-                        <option value="H">H</option>
-                    </select>
+                <label for="tonation-name-select-task-${id}">Tonacja: </label>
+                <select id="tonation-name-select-task-${id}" class="smaller-select">
+                    <option value="C">C</option>
+                    <option value="D">D</option>
+                    <option value="E">E</option>
+                    <option value="F">F</option>
+                    <option value="G">G</option>
+                    <option value="A">A</option>
+                    <option value="H">H</option>
+                </select>
                     
-                    <label for="accidental-task-${id}"></label>
-                    <select id="accidental-task-${id}" class="smaller-select">
-                        <option value=" "> </option>
-                        <option value="#">#</option>
-                        <option value="b">b</option>
-                    </select>
+                <label for="accidental-task-${id}"></label>
+                <select id="accidental-task-${id}" class="smaller-select">
+                    <option value=" "> </option>
+                    <option value="#">#</option>
+                    <option value="b">b</option>
+                </select>
 
-                    <label for="mode-task-${id}"></label>
-                    <select id="mode-task-${id}">
-                        <option value="dur">Dur</option>
-                        <option value="moll">moll</option>
-                    </select>
-                </div>
-            </form>
+                <label for="mode-task-${id}"></label>
+                <select id="mode-task-${id}">
+                    <option value="dur">Dur</option>
+                    <option value="moll">moll</option>
+                </select>
+            </div>
 
             <div id="components-place-${id}" class="components-place">
                 
             </div>
 
             <div class="task-submit">
-                <form>
-                    <input type="button" value="Zatwierdź" id="submit-${id}">
-                    <input type="button" value="Wyczyść" id="clear-${id}">
-                </form>
+                <input type="button" value="Zatwierdź" id="submit-${id}">
+                <input type="button" value="Wyczyść" id="clear-${id}">
+                <input type="button" value="Usuń" id="delete-${id}">
             </div>        
         </div>
     `;
