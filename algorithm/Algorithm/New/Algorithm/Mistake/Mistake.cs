84298a2d2b2fad6e6d2f88a56732f33ccb2cef45
przemekkojs <przemek.kojs@gmail.com>
2024-11-05 17:54:47 +0100

    Task checking implementation
    And it works flawlessly!
    
    Next steps will be implementing other rules, but it's too much for now.

--- algorithm/Algorithm/New/Algorithm/Mistake/Mistake.cs
+++ algorithm/Algorithm/New/Algorithm/Mistake/Mistake.cs
@@ -2,7 +2,8 @@
 {
     public abstract class Mistake
     {
-        public string Description { get; protected set; }
+        public string Description { get; protected set; } = "Niesprecyzowany błąd.";
+        public abstract int Quantity { get; }
 
         public abstract void GenerateDescription();
     }
