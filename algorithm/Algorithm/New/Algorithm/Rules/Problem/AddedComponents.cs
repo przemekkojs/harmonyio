75c3f16a488018add19efc43c1a566130e77c4ca
przemekkojs <przemek.kojs@gmail.com>
2024-12-02 10:32:37 +0100

    Added rule structure

---
+++ algorithm/Algorithm/New/Algorithm/Rules/Problem/AddedComponents.cs
@@ -0,0 +1,17 @@
+﻿using Algorithm.New.Music;
+
+namespace Algorithm.New.Algorithm.Rules.Problem
+{
+    public class AddedComponents : Rule
+    {
+        public AddedComponents() : base(
+            name: "Poprawne składniki dodane",
+            description: "Wszystkie ograniczenia narzucone na składniki dodane.",
+            oneFunction: true) { }
+
+        public override bool IsSatisfied(params Function[] functions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
