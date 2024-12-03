7b492ae8854a8209092e222e9c549d76e70a4331
przemekkojs <przemek.kojs@gmail.com>
2024-12-03 14:44:10 +0100

    Note and chord mapping
    From algorithm notes and stacks to midi notes and chords

--- midi/MidiPlayback/FileCreator.cs
+++ midi/MidiPlayback/FileCreator.cs
@@ -1,5 +1,7 @@
-﻿using Melanchall.DryWetMidi.Common;
+﻿using Algorithm.New.Algorithm;
+using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.MusicTheory;
 
 namespace MidiPlayback
@@ -9,14 +11,13 @@ namespace MidiPlayback
         private const int SPACE_BETWEEN_CHORDS = 100;
         private const int CHORD_DURATION = 500;
 
-        public static byte[] Create()
+        public static byte[] Create(Solution solution)
         {
             var midiFile = new MidiFile();
-
             var trackChunk = new TrackChunk();
-            midiFile.Chunks.Add(trackChunk);
+            long currentTime = 0;
 
-            long currentTime = 0;            
+            midiFile.Chunks.Add(trackChunk);
 
             var cMajor = new[] { NoteName.C, NoteName.E, NoteName.G };
             var fMajor = new[] { NoteName.F, NoteName.A, NoteName.C };
@@ -31,11 +32,75 @@ namespace MidiPlayback
             return midiData;
         }
 
-        private static void AddChord(TrackChunk trackChunk, NoteName[] notes, int octave, long duration, long startTime)
+        private static List<(NoteName, int)> CreateChord(Algorithm.New.Music.Stack stack)
+        {
+            List<(NoteName, int)> result = [];
+            var stackNotes = stack.Notes;
+
+            foreach(var note in stackNotes)
+            {
+                if (note == null)
+                    continue;
+
+                var midiNote = NoteToMidiNote(note);
+                result.Add(midiNote);
+            }
+
+            return result;
+        }
+
+        private static (NoteName, int) NoteToMidiNote(Algorithm.New.Music.Note note)
+        {
+            var octave = note.Octave;
+            var name = note.Name;
+
+            var noteName = name switch
+            {
+                "B#" => NoteName.C,
+                "C" => NoteName.C,
+                "Dbb" => NoteName.C,
+                "C#" => NoteName.CSharp,
+                "Db" => NoteName.CSharp,
+                "Cx" => NoteName.D,
+                "D" => NoteName.D,
+                "Ebb" => NoteName.D,
+                "D#" => NoteName.DSharp,
+                "Eb" => NoteName.DSharp,
+                "Fbb" => NoteName.DSharp,
+                "Dx" => NoteName.E,
+                "Fb" => NoteName.E,
+                "E" => NoteName.E,
+                "E#" => NoteName.F,
+                "F" => NoteName.F,
+                "Gbb" => NoteName.F,
+                "Ex" => NoteName.FSharp,
+                "F#" => NoteName.FSharp,
+                "Gb" => NoteName.FSharp,
+                "Fx" => NoteName.G,
+                "G" => NoteName.G,
+                "Abb" => NoteName.G,
+                "G#" => NoteName.GSharp,
+                "Ab" => NoteName.GSharp,
+                "A" => NoteName.A,
+                "Gx" => NoteName.A,
+                "Bbb" => NoteName.A,
+                "A#" => NoteName.ASharp,
+                "Bb" => NoteName.ASharp,
+                "Ax" => NoteName.B,
+                "Cb" => NoteName.B,
+                "B" => NoteName.B,
+                _ => NoteName.B
+            };
+
+            return (noteName, octave);
+        }
+
+        private static void AddChord(
+            TrackChunk trackChunk, NoteName[] notes, int octave, long duration, long startTime)
         {
             foreach (var noteName in notes)
             {
-                var noteNumber = (SevenBitNumber)(12 * (octave + 1) + (int)noteName); // Calculate MIDI note number
+                var noteNumber = (SevenBitNumber)(12 * (octave + 1) + (int)noteName);
                 trackChunk.Events.Add(new NoteOnEvent(noteNumber, (SevenBitNumber)100) { DeltaTime = startTime });
                 trackChunk.Events.Add(new NoteOffEvent(noteNumber, (SevenBitNumber)0) { DeltaTime = duration });
                 startTime = 0;
