424bb921aa8d140a6cfcd4f375cb34656ab2430a
przemekkojs <przemek.kojs@gmail.com>
2024-12-05 12:10:13 +0100

    Added description
    Time to implement
    
    After implementation, there is need to add IDs to ProblemRules, then generate ProblemMistakes and display them in popup

--- algorithm/Algorithm/New/Algorithm/Rules/Problem/AddedVsPositionAndRoot.cs
+++ algorithm/Algorithm/New/Algorithm/Rules/Problem/AddedVsPositionAndRoot.cs
@@ -5,8 +5,8 @@ namespace Algorithm.New.Algorithm.Rules.Problem
     public class AddedVsPositionAndRoot : Rule
     {
         public AddedVsPositionAndRoot() : base(
-            name: "",
-            description: "",
+            name: "Składniki dodane a pozycja i oparcie",
+            description: "Pozycja i oparcie musi być zgodna ze składnikami dodanymi",
             oneFunction: true) { }
 
         public override bool IsSatisfied(params Function[] functions)
