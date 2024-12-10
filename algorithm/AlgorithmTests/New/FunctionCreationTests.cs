0dd671138078017ca2f98288afb563c850dcdc6a
przemekkojs <przemek.kojs@gmail.com>
2024-12-10 17:54:13 +0100

    Mistake codes fix

---
+++ algorithm/AlgorithmTests/New/FunctionCreationTests.cs
@@ -0,0 +1,75 @@
+﻿using Algorithm.New.Music;
+using Index = Algorithm.New.Music.Index;
+
+namespace AlgorithmTests.New
+{
+    public class FunctionCreationTests
+    {
+        [Fact]
+        public void TTtest()
+        {
+            var tonation = Tonation.CMajor;
+
+            Function function = new(
+                minor: false,
+                symbol: Symbol.T,
+                tonation: tonation,
+                index: new Index()
+            );
+        }
+
+        [Fact]
+        public void SiiTtest()
+        {
+
+        }
+
+        [Fact]
+        public void TiiiTtest()
+        {
+
+        }
+
+        [Fact]
+        public void DiiiTtest()
+        {
+
+        }
+
+        [Fact]
+        public void STtest()
+        {
+
+        }
+
+        [Fact]
+        public void DTtest()
+        {
+
+        }
+
+        [Fact]
+        public void TviTtest()
+        {
+
+        }
+
+        [Fact]
+        public void SviTtest()
+        {
+
+        }
+
+        [Fact]
+        public void DviiTtest()
+        {
+
+        }
+
+        [Fact]
+        public void SviiTtest()
+        {
+
+        }
+    }
+}
